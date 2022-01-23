c3847e81202e43d81dd0a856a4e558e299d727ef
Esa Varemo <git@esav.fi>
2022-01-23 14:58:57 +0200

    Implement basic serializing/deserializing

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,66 @@
-use std::{io::{BufReader, Write, BufRead}, net::TcpStream, thread};
+use serde::{Deserialize, Serialize};
+use serde_json::de::Deserializer;
+use serde_json::json;
+use serde_json::Value;
+use std::{
+    io::{Write},
+    net::TcpStream,
+};
 
 struct JsonRpcConnection {
     stream: TcpStream,
-    reader: BufReader<TcpStream>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+#[serde(deny_unknown_fields)]
+pub struct Request {
+    pub id: Value,
+    pub method: String,
+    pub params: Option<Value>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+pub struct Response {
+    pub result: Value,
+    pub error: Value,
+    pub id: Value,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum Message {
+    /// An RPC request.
+    Request(Request),
+    /// A response to a Request.
+    Response(Response),
 }
 
 impl JsonRpcConnection {
     fn new(host: &str, port: u16) -> JsonRpcConnection {
         let stream = TcpStream::connect((host, port)).expect("Failed to connect");
-        let reader = BufReader::new(stream.try_clone().unwrap());
-        JsonRpcConnection { stream, reader }
+        JsonRpcConnection { stream }
     }
 
-    fn request(&mut self, method: &str, params: &str) {
-        let mut payload = String::from(r#"{"id": 1, "method": "get_schema", "params": ["_Server"]}"#);
-        payload.push('\n');
-        self.stream.write(payload.as_bytes()).unwrap();
-        loop {
-            let mut buf = String::new();
-            if self.reader.read_line(&mut buf).unwrap() == 0 {
-                break;
-            }
-
-            println!("{buf}");
-        }
-    }
-
-    fn close(self) {
-        self.stream.shutdown(std::net::Shutdown::Both).unwrap();
+    fn request(&mut self, method: &str, params: Option<Value>) {
+        let payload = Request {
+            id: 1.into(),
+            method: method.into(),
+            params,
+        };
+        //let mut payload = String::from(r#"{"id": 1, "method": "get_schema", "params": ["_Server"]}"#);
+        //payload.push('\n');
+        self.stream
+            .write(&serde_json::to_vec(&payload).unwrap())
+            .unwrap();
+        self.stream.write(b"\n").unwrap();
+        let deserializer = Deserializer::from_reader(self.stream.try_clone().unwrap());
+        let decoded: Message = deserializer.into_iter().next().unwrap().unwrap();
+        println!("{decoded:?}");
     }
 }
 
 fn main() {
-    println!("Hello, world!");
-
     let mut connection = JsonRpcConnection::new("127.0.0.1", 6641);
-    connection.request("get_schema", r#"["_Server"]"#);
-    connection.close();
-
-    
-    // get_schema", ["_Server"]
+    let params = vec!["_Server"];
+    connection.request("get_schema", Some(json!(params)));
 }
